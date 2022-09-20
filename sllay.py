9cd3195b2a7c80aa14d267d26b76211fcd6f36e1
Nixon Showalter <nixonshowalter@yahoo.com>
2022-09-19 22:49:36 -0400

    Worked on NOM1 function

--- sllay.py
+++ sllay.py
@@ -25,31 +25,54 @@ class Player():
         self.ID = ID
         self.relationships = relationships
 
+    def update_relationships(self, new_relationships):
+        self.relationships = new_relationships
+
+
+# player info: name, compitition ability, gender, ID, relationship values
 
 player1 = Player("Paula", 2, "female", 1, {
                  'p2': 0, 'p3': 0, 'p4': 0, 'p5': 0, 'p6': 0, 'p7': 0, 'p8': 0, 'p9': 0, 'p10': 0})
+
 player2 = Player("Anthony", 1, "male", 2, {
                  'p1': 0, 'p2': 0, 'p4': 0, 'p5': 0, 'p6': 0, 'p7': 0, 'p8': 0, 'p9': 0, 'p10': 0})
+
 player3 = Player("Leonard", 5, "male", 3, {
                  'p1': 0, 'p2': 0, 'p3': 0, 'p5': 0, 'p6': 0, 'p7': 0, 'p8': 0, 'p9': 0, 'p10': 0})
+
 player4 = Player("Krista", 4, "female", 4, {
                  'p2': 0, 'p3': 0, 'p4': 0, 'p5': 0, 'p6': 0, 'p7': 0, 'p8': 0, 'p9': 0, 'p10': 0})
+
 player5 = Player("Nico", 3, "male", 5, {
                  'p1': 0, 'p2': 0, 'p3': 0, 'p4': 0, 'p6': 0, 'p7': 0, 'p8': 0, 'p9': 0, 'p10': 0})
+
 player6 = Player("Alex", 3, "female", 6, {
                  'p1': 0, 'p2': 0, 'p3': 0, 'p4': 0, 'p5': 0, 'p7': 0, 'p8': 0, 'p9': 0, 'p10': 0})
+
 player7 = Player("Izabella", 5, "female", 7, {
                  'p1': 0, 'p2': 0, 'p3': 0, 'p4': 0, 'p5': 0, 'p6': 0, 'p8': 0, 'p9': 0, 'p10': 0})
+
 player8 = Player("Ken", 4, "other", 8, {
                  'p1': 0, 'p2': 0, 'p3': 0, 'p4': 0, 'p5': 0, 'p6': 0, 'p7': 0, 'p9': 0, 'p10': 0})
+
 player9 = Player("Lavi", 1, "male", 9, {
                  'p1': 0, 'p2': 0, 'p3': 0, 'p4': 0, 'p5': 0, 'p6': 0, 'p7': 0, 'p8': 0, 'p10': 0})
+
 player10 = Player("Axe", 2, "other", 10, {
                   'p1': 0, 'p2': 0, 'p3': 0, 'p4': 0, 'p5': 0, 'p6': 0, 'p7': 0, 'p8': 0, 'p9': 0})
 
 
-# print(player1relationships.values())
-print(min(player1.relationships.values()))
+player1.update_relationships({'p1': 0, 'p2': 0, 'p3': 0, 'p4': 0, 'p5': 0, 'p6': 0, 'p7': 0, 'p8': 0, 'p9': 0})
+player2.update_relationships({'p1': 0, 'p2': 0, 'p3': 0, 'p4': 0, 'p5': 0, 'p6': 0, 'p7': 0, 'p8': 0, 'p9': 0})
+player3.update_relationships({'p1': 0, 'p2': 0, 'p3': 0, 'p4': 0, 'p5': 0, 'p6': 0, 'p7': 0, 'p8': 0, 'p9': 0})
+player4.update_relationships({'p1': 0, 'p2': 0, 'p3': 0, 'p4': 0, 'p5': 0, 'p6': 0, 'p7': 0, 'p8': 0, 'p9': 0})
+player5.update_relationships({'p1': 0, 'p2': 0, 'p3': 0, 'p4': 0, 'p5': 0, 'p6': 0, 'p7': 0, 'p8': 0, 'p9': 0})
+player6.update_relationships({'p1': 0, 'p2': 0, 'p3': 0, 'p4': 0, 'p5': 0, 'p6': 0, 'p7': 0, 'p8': 0, 'p9': 0})
+player7.update_relationships({'p1': 0, 'p2': 0, 'p3': 0, 'p4': 0, 'p5': 0, 'p6': 0, 'p7': 0, 'p8': 0, 'p9': 0})
+player8.update_relationships({'p1': 0, 'p2': 0, 'p3': 0, 'p4': 0, 'p5': 0, 'p6': 0, 'p7': 0, 'p8': 0, 'p9': 0})
+player9.update_relationships({'p1': 0, 'p2': 0, 'p3': 0, 'p4': 0, 'p5': 0, 'p6': 0, 'p7': 0, 'p8': 0, 'p9': 0})
+player10.update_relationships({'p1': 0, 'p2': 0, 'p3': 0, 'p4': 0, 'p5': 0, 'p6': 0, 'p7': 0, 'p8': 0, 'p9': 0})
+
 
 L = [player1, player2, player3, player4, player5,
      player6, player7, player8, player9, player10]
@@ -81,17 +104,18 @@ def HOH():
             return player
 
 
+# find lowest relationship: min(hoh.relationships.values())
 def NOM1(hoh):
-    n = 0
-    relationships = hoh.relationships.keys
-    for player in {hoh.relationships}:
-        n += {hoh.relationships}.key
-    if n > 1:
-        print(random.randint(1, n))
-    if n <= 1:
-        print(player.name)
-        return player
+    lowest_relationship = min(hoh.relationships.values())
+    possible_noms = []
+
+    if hoh.relationships.values().counter(lowest_relationship) > 1:
+        for player in hoh.relationships.keys():
+            if hoh.relationships[player] == lowest_relationship:
+                possible_noms.append(player)
+        random_nom = random.randrange(0, (possible_noms.count() + 1))
+        
+
+        
 
 
-HOHWINNER = HOH()
-NOM1(HOHWINNER)
